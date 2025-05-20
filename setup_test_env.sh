78fdc9becf686961c13cfc7d974e4b5b37bf88aa
Carlos Rodrigues <crodrigues@cloudflare.com>
2025-05-20 18:49:26 +0100

    Fix test suite, test credentials option

--- setup_test_env.sh
+++ setup_test_env.sh
@@ -4,18 +4,18 @@ set -e
 
 
 # install chrome
-wget -nc https://dl-ssl.google.com/linux/linux_signing_key.pub 
-cat linux_signing_key.pub | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/linux_signing_key.gpg  >/dev/null 
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/chrome.list' 
-sudo apt update 
-sudo apt install google-chrome-stable 
+wget -nc https://dl-ssl.google.com/linux/linux_signing_key.pub
+cat linux_signing_key.pub | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/linux_signing_key.gpg  >/dev/null
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/chrome.list'
+sudo apt update
+sudo apt install google-chrome-stable
 
 
 # pyodide build and test
-pip install pytest 
-pip install pyodide-build pytest-pyodide 
+pip install pytest
+pip install pyodide-build pytest-pyodide
 # install chromedriver stuff for selenium to control chrome
-pip install selenium webdriver-manager 
+pip install selenium webdriver-manager
 pip install chromedriver-binary-auto
 # make sure chromedriver is on path
 export PATH=$PATH:`chromedriver-path`
@@ -23,6 +23,8 @@ export PATH=$PATH:`chromedriver-path`
 # run the tests
 cd tests
 # get pyodide to tests/pyodide
-wget https://github.com/pyodide/pyodide/releases/download/0.21.0/pyodide-build-0.21.0.tar.bz2
-tar xjf pyodide-build-0.21.0.tar.bz2
-pytest . --dist-dir ./pyodide --rt chrome -v 
+wget https://github.com/pyodide/pyodide/releases/download/0.25.1/pyodide-0.25.1.tar.bz2
+tar xjf pyodide-0.25.1.tar.bz2
+cp "$(python3 -c 'import os.path; import pytest_pyodide; print(os.path.dirname(pytest_pyodide.__file__))')/_templates/test.html" pyodide/
+cp "$(python3 -c 'import os.path; import pytest_pyodide; print(os.path.dirname(pytest_pyodide.__file__))')/_templates/webworker_dev.js" pyodide/
+pytest . --dist-dir ./pyodide --rt chrome -v
--- tests/test_non_streaming.py
+++ tests/test_non_streaming.py
@@ -1,8 +1,9 @@
 from pathlib import Path
+import os.path
 import glob
 
 from pytest_pyodide import run_in_pyodide, spawn_web_server
-from pytest import fixture
+from pytest import fixture, fail
 
 
 @fixture(scope="module")
@@ -43,6 +44,9 @@ def _install_package(selenium, base_url):
             import requests
             """
         )
+        break
+    else:
+        fail(f"no pre-built *.whl found in {os.path.relpath(wheel_folder)}")
 
 
 def test_install_package(selenium_standalone, web_server_base):
@@ -56,15 +60,52 @@ def test_requests_get(selenium_standalone, dist_dir, web_server_base):
     def test_fn(selenium_standalone, base_url):
         import requests
 
+        import pyodide_http._requests
+        assert pyodide_http._requests._IS_PATCHED
+
+        import pyodide_http as ph
+
         print("get:", base_url)
-        url = f"{base_url}/yt-4.0.4-cp310-cp310-emscripten_3_1_14_wasm32.whl"
-        resp = requests.get(url)
+        url = f"{base_url}/yt-4.1.4-cp311-cp311-emscripten_3_1_46_wasm32.whl"
+
+        # The test web server sets "Access-Control-Allow-Origin: *" which disallows sending credentials.
+        # Credentials are explicitly disabled, although that's the default, to exercise the option code.
+        with ph.option_context(with_credentials=False):
+            resp = requests.get(url)
+
         data = resp.content
         assert resp.request.url == url
 
         return len(data)
 
-    assert test_fn(selenium_standalone, f"{web_server_base}{dist_dir}/") == 11373926
+    assert test_fn(selenium_standalone, f"{web_server_base}{dist_dir}/") == 78336150
+
+
+def test_urllib_get(selenium_standalone, dist_dir, web_server_base):
+    _install_package(selenium_standalone, web_server_base)
+
+    @run_in_pyodide
+    def test_fn(selenium_standalone, base_url):
+        import urllib.request
+
+        import pyodide_http._urllib
+        assert pyodide_http._urllib._IS_PATCHED
+
+        import pyodide_http as ph
+
+        # The test web server sets "Access-Control-Allow-Origin: *" which disallows sending credentials.
+        # Credentials are explicitly disabled, although that's the default, to exercise the option code.
+        ph.set_with_credentials_option(False)
+
+        print("get:", base_url)
+        url = f"{base_url}/yt-4.1.4-cp311-cp311-emscripten_3_1_46_wasm32.whl"
+        with urllib.request.urlopen(url) as resp:
+            data = resp.read()
+            assert resp.url == url
+
+            return len(data)
+
+    assert test_fn(selenium_standalone, f"{web_server_base}{dist_dir}/") == 78336150
 
 
 def test_requests_404(selenium_standalone, dist_dir, web_server_base):
@@ -73,6 +114,9 @@ def test_requests_404(selenium_standalone, dist_dir, web_server_base):
     @run_in_pyodide
     def test_fn(selenium_standalone, base_url):
         import requests
+
+        import pyodide_http._requests
+        assert pyodide_http._requests._IS_PATCHED
 
         print("get:", base_url)
         resp = requests.get(f"{base_url}/surely_this_file_does_not_exist.hopefully.")
